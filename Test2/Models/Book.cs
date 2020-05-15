aaaf949856f14324e0e2e65e9beda48f5813c807
Cristina <cristina.ilea2@yahoo.com>
2020-05-15 22:16:35 +0300

    Add project files.

---
+++ Test2/Models/Book.cs
@@ -0,0 +1,13 @@
+﻿
+namespace Models
+{
+
+    public class Book
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string Author { get; set; }
+        public int YearOfPublication { get; set; }
+        public double Price { get; set; }
+    }
+}
---
+++ Test2/Startup.cs
@@ -0,0 +1,48 @@
+// Unused usings removed
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Test2.Models;
+using FileContextCore;
+
+namespace Test2
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+           services.AddDbContext<BookContext>(opt =>
+           opt.UseFileContextDatabase("Books"));
+           services.AddControllers();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
